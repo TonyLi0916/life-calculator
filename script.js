e1f188e81027c95a2b6084c71ef0805fc2c6978d
Tony Li <tli689@uwo.ca>
2024-11-14 01:54:56 -0500

    change

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function calculateSingleTip() {
     const amount = parseFloat(document.getElementById("tipAmount").value);
     const percentage = parseFloat(document.getElementById("percentageAmount").value);
 
-    if (!isNaN(amount) && !isNaN(percentage) && amount >= 0 && percentage >= 0 && percentage <= 100) {
+    if (!isNaN(amount) && !isNaN(percentage) && amount >= 0 && percentage >= 0) {
         const tip = (amount * (percentage / 100)).toFixed(2);
         document.getElementById("singleResult").innerHTML = `Tip Amount : $${tip}`;
     }
@@ -18,7 +18,7 @@ function calculateSplitTip(){
     const percentage = parseFloat(document.getElementById("percentageAmount2").value);
     const people = parseFloat(document.getElementById("numberPeople").value);
 
-    if (!isNaN(amount) && !isNaN(percentage) && !isNaN(people) && amount >= 0 && percentage >= 0 && percentage <= 100 && people >= 1) {
+    if (!isNaN(amount) && !isNaN(percentage) && !isNaN(people) && amount >= 0 && percentage >= 0 && people >= 1) {
         const split = (amount / people).toFixed(2);
         const tip = (split * (percentage / 100)).toFixed(2);
         document.getElementById("splitResult").innerHTML = `Tip Amount : $${tip} per person`;
