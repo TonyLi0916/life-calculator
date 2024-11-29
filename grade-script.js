17a171404a4cf224917ca474acc097a050d234d2
Tony Li <tli689@uwo.ca>
2024-11-29 02:45:52 -0500

    changes

--- grade-script.js
+++ grade-script.js
@@ -2,12 +2,22 @@ function calculateFinalGrade() {
     const currentGrade = parseFloat(document.getElementById("currentGrade").value);
     const examWeight = parseFloat(document.getElementById("examWeight").value);
     const desiredGrade = parseFloat(document.getElementById("desiredGrade").value);
+    const neededGrade = ((desiredGrade - (currentGrade * (1 - examWeight / 100))) / (examWeight / 100)).toFixed(2);
 
     if (!isNaN(currentGrade) && !isNaN(examWeight) && !isNaN(desiredGrade) && examWeight > 0 && examWeight <= 100) {
-        const neededGrade = ((desiredGrade - (currentGrade * (1 - examWeight / 100))) / (examWeight / 100)).toFixed(2);
         document.getElementById("gradeResult").innerHTML = `You need a ${neededGrade}% on the final exam to achieve your desired grade.`;
     } else {
         document.getElementById("gradeResult").innerHTML = "Please enter valid values.";
+    }
+
+    if (neededGrade > 100){
+        document.getElementById("gradeMessage").innerHTML = "Unfortunately, it is impossible to achieve the desired grade:("
+    }
+    else if (neededGrade < 0){
+        document.getElementById("gradeMessage").innerHTML = "You have already secured the desired grade:)"
+    }
+    else{
+        document.getElementById("gradeMessage").innerHTML = "Good luck! You got this."
     }
 }
 
